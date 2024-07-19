46ae2ae7a0d35f31b4e256749b69eeded04555ea
tuppercm <tup319@gmail.com>
2024-07-19 11:01:08 -0400

    Added footer
    Added content, links and styling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,25 @@ function App() {
       <header className="App-header">
         <h1>New Weather App</h1>
       </header>
+      <footer>
+        Coded in 2024 by Caitlin Tupper, open-sourced in
+        <a
+          href="https://github.com/tuppercm/weather-app-new"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://ctup-weather-react-app-new.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+        .
+      </footer>
     </div>
   );
 }
