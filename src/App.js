6d2126be9d94c6c2adc59c5128386fb06c779a12
tuppercm <tup319@gmail.com>
2024-07-19 10:47:56 -0400

    Cleaned up template
    Removed unnecessary files and CSS from the React starter template

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>My New Weather App</h1>
+        <h1>New Weather App</h1>
       </header>
     </div>
   );
