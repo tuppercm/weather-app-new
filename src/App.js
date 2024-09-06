75b12b0d520cdfe286350bf737259a4564fdac4f
tuppercm <tup319@gmail.com>
2024-09-06 09:38:19 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,28 +3,30 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>New Weather App</h1>
-      </header>
-      <footer>
-        Coded in 2024 by Caitlin Tupper, open-sourced on{" "}
-        <a
-          href="https://github.com/tuppercm/weather-app-new"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-        , and hosted on{" "}
-        <a
-          href="https://ctup-weather-react-app-new.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-        .
-      </footer>
+      <div class="container">
+        <header className="App-header">
+          <h1>New Weather App</h1>
+        </header>
+        <footer>
+          Coded in 2024 by Caitlin Tupper, open-sourced on{" "}
+          <a
+            href="https://github.com/tuppercm/weather-app-new"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          , and hosted on{" "}
+          <a
+            href="https://ctup-weather-react-app-new.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
