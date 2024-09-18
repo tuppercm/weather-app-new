49a81f0a2074a938390324f58493c75d35f69132
tuppercm <tup319@gmail.com>
2024-09-18 13:59:07 -0400

    Added stying to WeatherForecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -7,9 +7,9 @@ export default function WeatherForecast() {
       <div className="row">
         <div className="col">
           <div className="forecast-day">Day</div>
-          <div>Icon</div>
+          <div className="forecast-icon">Icon</div>
           <div className="forecast-temperature">
-            <span className="forecast-high">100°</span> |{" "}
+            <span className="forecast-high">100°</span>
             <span className="forecast-low">-10°</span>
           </div>
         </div>
