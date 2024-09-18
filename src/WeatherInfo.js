da46c4ea4b5b015736c795f02fb74f00794f45c3
tuppercm <tup319@gmail.com>
2024-09-18 13:39:51 -0400

    Added Celsius and Fahrenheit display options

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
+
 
 export default function WeatherInfo(props) {
   return (
@@ -14,8 +16,8 @@ export default function WeatherInfo(props) {
       <div className="row">
         <div className="col-6">
           <img src={props.data.iconUrl} alt={props.data.icon} />
-          <span className="temperature">{Math.round(props.data.temperature)}</span>
-          <span className="unit">°C</span>
+          <WeatherTemperature celsius={props.data.temperature}/>
+        
         </div>
         <div className="col-6">
           <ul>
